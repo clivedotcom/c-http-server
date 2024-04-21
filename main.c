7de83f4af2125cee87bed5348aa009b2e8b2940b
Clive <clive@surgedesigns.co.uk>
2024-04-21 07:27:24 +0000

    Added reading from a file

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
-#define PORT 8088
 #define BUFFER_SIZE 1024
 #define MAX_REQUEST_SIZE 1024
-#define DEFAULT_DIR "/var/www/"
+#define DEFAULT_DIR "/var/www"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,20 +8,39 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+int PORT = 8088;
+char* requested_file;
+
+// Method to load a HTML resource
+char* load_html_resource(char* filename) {
+
+}
+
+//Method to build the file name
+char* build_file_name(char* filename) {
+    // Calculate the size of the variable required
+    char dest[strlen(DEFAULT_DIR)+strlen(filename)];
+    // Copy in default DIR
+    strcpy( dest, DEFAULT_DIR );
+    // Copy in Filename
+    strcat( dest, filename );
+    // Load the returned value into buffer
+    requested_file = dest;
+    // Return value
+    return requested_file;
+}
 
 // Method to check if a requested file exists
 int file_exists(char* filename) {
     // Check if the file exists by testing for read access
-char dest[strlen(DEFAULT_DIR)+strlen(filename)];
-strcpy( dest, DEFAULT_DIR );
-strcat( dest, filename );
-    if (access(dest, F_OK) != -1) {
-        // File exists
-        return 1;
-    } else {
-        // File doesn't exist
-        return 0;
-    }
+    char* dest = build_file_name(filename);
+        if (access(dest, F_OK) != -1) {
+            // File exists
+            return 1;
+        } else {
+            // File doesn't exist
+            return 0;
+        }
 }
 
 // Method to extract the method & requested resource
@@ -57,17 +75,49 @@ void *connection_handler(void *socket_desc) {
     valread = read(sock, buffer, BUFFER_SIZE);
     extract_method_and_url(buffer, method, url);
     char response[1024];
-      if(file_exists(url))
+    if(file_exists(url))
     {
-        // File exists
-        snprintf(response, sizeof(response), "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1>Method: %s<br /> URL %s <br /><a href=\"page.html\">Another page...</a></body></html>", method, url);
-    
+    // File exists so read the file into buffer
+        char* filename = build_file_name(url);
+        char buf[0x100];
+        snprintf(buf, sizeof(buf), "%s", filename);
+        char * buffer = 0;
+        FILE *f;
+        long length;
+        f = fopen(buf, "r");
+        if (f)
+        {
+        fseek (f, 0, SEEK_END);
+        length = ftell (f);
+        fseek (f, 0, SEEK_SET);
+        buffer = malloc (length);
+        if (buffer)
+        {
+            fread (buffer, 1, length, f);
+        }
+        fclose (f);
+        }
+        if (buffer)
+        {
+    // Calculate the size of the variable required
+    char content[strlen("HTTP/1.1 200 OK\nContent-Type: text/html\n\n")+strlen(buffer)];
+    // Copy in default DIR
+    strcpy( content, "HTTP/1.1 200 OK\nContent-Type: text/html\n\n" );
+    // Copy in Filename
+    strcat( content, buffer );
+    // Load the returned value into buffer
+    snprintf(response, sizeof(response), content, method, url);
+
+    buffer="";
+        }
+
+//snprintf(response, sizeof(response), "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1>Method: %s<br /> URL %s <br /><a href=\"page.html\">Another page...</a></body></html>", method, url);
+
     }
     else
     {
-        // File doesn't Exists.
-        snprintf(response, sizeof(response), "HTTP/1.1 404 OK\nContent-Type: text/html\n\n<html><body><h1>404 Error!</h1>Page %s not found...</body></html>", url);
-    
+    // File doesn't Exists.
+    snprintf(response, sizeof(response), "HTTP/1.1 404 OK\nContent-Type: text/html\n\n<html><body><h1>404 Error!</h1>Page %s not found...</body></html>", url);
     }
     // Send the response
     write(sock, response, strlen(response));
@@ -89,12 +139,24 @@ int main() {
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    // Bind the socket to localhost port 8088
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        printf("Bind failed");
-        exit(EXIT_FAILURE);
-    }
+
+    // Got bored of the code not releasing the port on exit, so this loops through to find a new port and outputs to console
+    int test_case = 0;
+    while(test_case == 0) {
+        address.sin_port = htons(PORT);
+        // Bind the socket to localhost port 8088
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+                printf("Bind failed");
+                //exit(EXIT_FAILURE);
+                PORT = PORT + 1;
+            }
+            else
+            {
+                test_case = 1;  
+                printf("PORT: %i",PORT); 
+            }
+        }
+
     // Listen for incoming connections
     if (listen(server_fd, 3) < 0) {
         printf("Listen failed");
